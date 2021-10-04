32fbcd3b4856a96fb657e684e31ff214556594be
Nicola <nicola.porta@spindox.it>
2021-10-04 17:20:06 +0200

    - refactoring - create Edge class

---
+++ xagon-react-app/src/models/Edge.ts
@@ -0,0 +1,32 @@
+import { Vector3 } from '@babylonjs/core';
+
+export type EdgeId = bigint;
+
+class Edge {
+  //
+  private edge: Vector3;
+
+  private edgePoints: Array<Vector3>;
+
+  private edgeId: EdgeId;
+
+  public constructor(id: EdgeId, a: Vector3, b: Vector3) {
+    this.edgeId = id;
+    this.edgePoints = [a, b];
+    this.edge = a.subtract(b);
+  }
+
+  public getEdge(): Vector3 {
+    return this.edge;
+  }
+
+  public getId(): EdgeId {
+    return this.edgeId;
+  }
+
+  public getCenterPoint(scaleFactor = 1): Vector3 {
+    return this.edgePoints[1].add(this.edge.scale(scaleFactor));
+  }
+}
+
+export default Edge;
--- xagon-react-app/src/models/Icosahedron.ts
+++ xagon-react-app/src/models/Icosahedron.ts
@@ -1,14 +1,22 @@
 import { Vector3 } from '@babylonjs/core';
 import Triangle from './Triangle';
+import Edge from './Edge';
 
 class Icosahedron {
   private triangles: Array<Triangle>;
 
   private triangleCount = 0n;
 
-  private generateId(): bigint {
+  private edgeCount = 0n;
+
+  private generateTriangleId(): bigint {
     this.triangleCount += 1n;
     return this.triangleCount;
+  }
+
+  private generateEdgeId(): bigint {
+    this.edgeCount += 1n;
+    return this.edgeCount;
   }
 
   public constructor() {
@@ -34,26 +42,36 @@ class Icosahedron {
     points.forEach((p: Vector3) => p.normalize());
 
     this.triangles = [
-      new Triangle(this.generateId(), points[0], points[1], points[2]),
-      new Triangle(this.generateId(), points[3], points[2], points[1]),
-      new Triangle(this.generateId(), points[3], points[4], points[5]),
-      new Triangle(this.generateId(), points[3], points[8], points[4]),
-      new Triangle(this.generateId(), points[0], points[6], points[7]),
-      new Triangle(this.generateId(), points[0], points[9], points[6]),
-      new Triangle(this.generateId(), points[4], points[10], points[11]),
-      new Triangle(this.generateId(), points[6], points[11], points[10]),
-      new Triangle(this.generateId(), points[2], points[5], points[9]),
-      new Triangle(this.generateId(), points[11], points[9], points[5]),
-      new Triangle(this.generateId(), points[1], points[7], points[8]),
-      new Triangle(this.generateId(), points[10], points[8], points[7]),
-      new Triangle(this.generateId(), points[3], points[5], points[2]),
-      new Triangle(this.generateId(), points[3], points[1], points[8]),
-      new Triangle(this.generateId(), points[0], points[2], points[9]),
-      new Triangle(this.generateId(), points[0], points[7], points[1]),
-      new Triangle(this.generateId(), points[6], points[9], points[11]),
-      new Triangle(this.generateId(), points[6], points[10], points[7]),
-      new Triangle(this.generateId(), points[4], points[11], points[5]),
-      new Triangle(this.generateId(), points[4], points[8], points[10]),
+      new Triangle(this.generateTriangleId(), points[0], points[1], points[2]),
+      new Triangle(this.generateTriangleId(), points[3], points[2], points[1]),
+      new Triangle(this.generateTriangleId(), points[3], points[4], points[5]),
+      new Triangle(this.generateTriangleId(), points[3], points[8], points[4]),
+      new Triangle(this.generateTriangleId(), points[0], points[6], points[7]),
+      new Triangle(this.generateTriangleId(), points[0], points[9], points[6]),
+      new Triangle(
+        this.generateTriangleId(),
+        points[4],
+        points[10],
+        points[11],
+      ),
+      new Triangle(
+        this.generateTriangleId(),
+        points[6],
+        points[11],
+        points[10],
+      ),
+      new Triangle(this.generateTriangleId(), points[2], points[5], points[9]),
+      new Triangle(this.generateTriangleId(), points[11], points[9], points[5]),
+      new Triangle(this.generateTriangleId(), points[1], points[7], points[8]),
+      new Triangle(this.generateTriangleId(), points[10], points[8], points[7]),
+      new Triangle(this.generateTriangleId(), points[3], points[5], points[2]),
+      new Triangle(this.generateTriangleId(), points[3], points[1], points[8]),
+      new Triangle(this.generateTriangleId(), points[0], points[2], points[9]),
+      new Triangle(this.generateTriangleId(), points[0], points[7], points[1]),
+      new Triangle(this.generateTriangleId(), points[6], points[9], points[11]),
+      new Triangle(this.generateTriangleId(), points[6], points[10], points[7]),
+      new Triangle(this.generateTriangleId(), points[4], points[11], points[5]),
+      new Triangle(this.generateTriangleId(), points[4], points[8], points[10]),
     ];
 
     computeAdjacentTriangles(this.triangles);
@@ -64,28 +82,25 @@ class Icosahedron {
   }
 
   private subdivideTriangle(triangle: Triangle): Array<Triangle> {
-    const s1 = triangle.p2().subtract(triangle.p1());
-    s1.x /= 2;
-    s1.y /= 2;
-    s1.z /= 2;
-    const ps1 = triangle.p1().add(s1);
+    const edge1 = new Edge(this.generateEdgeId(), triangle.p2(), triangle.p1());
+    const center1 = edge1.getCenterPoint(0.5);
 
-    const s2 = triangle.p3().subtract(triangle.p2());
-    s2.x /= 2;
-    s2.y /= 2;
-    s2.z /= 2;
-    const ps2 = triangle.p2().add(s2);
+    const edge2 = new Edge(this.generateEdgeId(), triangle.p3(), triangle.p2());
+    const center2 = edge2.getCenterPoint(0.5);
 
-    const s3 = triangle.p1().subtract(triangle.p3());
-    s3.x /= 2;
-    s3.y /= 2;
-    s3.z /= 2;
-    const ps3 = triangle.p3().add(s3);
+    const edge3 = new Edge(this.generateEdgeId(), triangle.p1(), triangle.p3());
+    const center3 = edge3.getCenterPoint(0.5);
 
-    const tr1 = new Triangle(this.generateId(), triangle.p1(), ps1, ps3);
-    const tr2 = new Triangle(this.generateId(), ps1, triangle.p2(), ps2);
-    const tr3 = new Triangle(this.generateId(), ps1, ps2, ps3);
-    const tr4 = new Triangle(this.generateId(), ps3, ps2, triangle.p3());
+    const scaleToOne = 1 / center1.length();
+
+    const p1 = center1.scale(scaleToOne);
+    const p2 = center2.scale(scaleToOne);
+    const p3 = center3.scale(scaleToOne);
+
+    const tr1 = new Triangle(this.generateTriangleId(), triangle.p1(), p1, p3);
+    const tr2 = new Triangle(this.generateTriangleId(), p1, triangle.p2(), p2);
+    const tr3 = new Triangle(this.generateTriangleId(), p1, p2, p3);
+    const tr4 = new Triangle(this.generateTriangleId(), p3, p2, triangle.p3());
 
     return [tr1, tr2, tr3, tr4];
   }
