d676503efe161b51e40a9becdebe819239b1e981
Nicola <nicola.porta@spindox.it>
2021-10-15 12:27:35 +0200

    +  bones' rotation for triangles correctly deformation

--- xagon-react-app/src/components/GameComponent.tsx
+++ xagon-react-app/src/components/GameComponent.tsx
@@ -20,6 +20,8 @@ const onSceneReady = (sceneArg: Scene) => {
   const alpha = -Math.PI / 2;
   const beta = Math.PI / 2.5;
   const radius = 3;
+  const angle90 = Math.PI / 2;
+  const angle120 = Math.PI / (6 / 4);
   const target = new Vector3(0, 0, 0);
   const camera = new ArcRotateCamera(
     'camera',
@@ -56,24 +58,27 @@ const onSceneReady = (sceneArg: Scene) => {
   ).then(({ meshes, skeletons }) => {
     if (meshes && meshes.length > 0) {
       const triangleMesh = meshes[0];
+      const TRIANGLE_RADIUS = 1;
+      const TRIANGLE_SIDE = TRIANGLE_RADIUS * (3 / Math.sqrt(3));
+      const TRIANGLE_SCALE = 0.9;
+      const triangleEdgeLength = icosahedron.findShortestEdgeLength();
+      const scalingRatio =
+        (1 / TRIANGLE_SIDE) * triangleEdgeLength * TRIANGLE_SCALE;
+
       // console.log('loaded', triangleMesh);
       // console.log('meshes', meshes);
 
-      if (skeletons && skeletons.length > 0) {
-        const skeleton = skeletons[0];
-        console.log('bones', skeleton.bones);
-        // skeleton.bones[0].scale(1, 1, 1);
-        // skeleton.bones[1].scale(1, 1, 1);
-        // skeleton.bones[2].scale(1, 1, 1);
-      } else {
-        console.warn('No skeletons found');
-      }
-      const triangleRadius = 1;
-      const triangleSide = triangleRadius * (3 / Math.sqrt(3));
-      const triangleEdgeLength = icosahedron.findShortestEdgeLength();
+      // if (skeletons && skeletons.length > 0) {
+      //   const skeleton = skeletons[0];
+      //   console.log('bones', skeleton.bones);
+      //   skeleton.bones[0].scale(1, 1, 1);
+      //   skeleton.bones[1].scale(1, 1, 1);
+      //   skeleton.bones[2].scale(1, 1, 1);
+      // } else {
+      //   console.warn('No skeletons found');
+      // }
 
-      triangles.map((tr, i) => {
-        const scalingRatio = (1 / triangleSide) * triangleEdgeLength;
+      triangles.forEach((tr, i) => {
         triangleMesh.scaling = new Vector3(
           scalingRatio,
           scalingRatio,
@@ -86,34 +91,56 @@ const onSceneReady = (sceneArg: Scene) => {
           const triangleCenter = tr.getCenterPoint();
           const direction = triangleCenter; // Center - origin
           meshClone.parent = meshNode;
-          meshNode.setDirection(direction, 0, Math.PI / 2, 0);
+          meshNode.setDirection(direction, 0, angle90, 0);
           meshClone.position = new Vector3(0, direction.length(), 0);
 
           // addAxisToScene({ scene, size: 1, parent: meshClone });
-
+          // addAxisToScene({ scene, size: 1, parent: meshNode });
           const p1CenterVector = tr.p1().subtract(triangleCenter);
+          const p2CenterVector = tr.p2().subtract(triangleCenter);
+          const p3CenterVector = tr.p3().subtract(triangleCenter);
 
           const angle = Vector3.GetAngleBetweenVectors(
             meshNode.forward,
             p1CenterVector,
             meshNode.up,
           );
-          console.log(angle);
-
+          //
           meshClone.rotate(meshClone.up, angle);
-        }
-        // BONES
-        //   if (meshClone && skeletons && triangleMesh.skeleton) {
-        //     meshClone.skeleton = triangleMesh.skeleton.clone(`skeleton${i}`);
+          if (meshClone && skeletons && triangleMesh.skeleton) {
+            meshClone.skeleton = triangleMesh.skeleton.clone(`skeleton${i}`);
+            const skeletonMesh = meshClone.skeleton;
 
-        //     const skeletonMesh = meshClone.skeleton;
-        //     skeletonMesh.bones[0].scale(1, 1, 1);
-        //     skeletonMesh.bones[1].scale(1, 1, 1);
-        //     skeletonMesh.bones[2].scale(1, 1, 1);
-        //   }
-        //   console.log(meshClone.skeleton);
-        // });
-        return meshClone;
+            // const angle2 = Vector3.GetAngleBetweenVectors(
+            //   p3CenterVector,
+            //   p1CenterVector,
+            //   meshNode.up,
+            // );
+            // const R2 = skeletonMesh.bones[2].rotation;
+            // R2.y -= angle2 + Math.PI / (6 / 4);
+
+            const RotationBone1 = skeletonMesh.bones[0].rotation;
+            const RotationBone2 = skeletonMesh.bones[1].rotation;
+
+            const angleP1ToP3 = Vector3.GetAngleBetweenVectors(
+              p1CenterVector,
+              p3CenterVector,
+              meshNode.up,
+            );
+            const angleP1ToP2 = Vector3.GetAngleBetweenVectors(
+              p1CenterVector,
+              p2CenterVector,
+              meshNode.up,
+            );
+
+            RotationBone1.y += angleP1ToP3 - angle120;
+            RotationBone2.y += angleP1ToP2 + angle120;
+
+            skeletonMesh.bones[0].setRotation(RotationBone1);
+            skeletonMesh.bones[1].setRotation(RotationBone2);
+            // skeletonMesh.bones[2].setRotation(R2);
+          }
+        }
       });
       triangleMesh.visibility = 0;
     }
