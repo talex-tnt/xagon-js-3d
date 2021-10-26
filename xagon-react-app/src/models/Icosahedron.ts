1207f351c94d06dc016a54fc48b5e38f1126d912
Nicola <nicola.porta@spindox.it>
2021-10-26 11:23:01 +0200

    - refactoring findEquilateralTriangle

--- xagon-react-app/src/models/Icosahedron.ts
+++ xagon-react-app/src/models/Icosahedron.ts
@@ -85,7 +85,7 @@ class Icosahedron {
 
     subTriangles.forEach((tr) => tr.setType(randomType()));
 
-    return [...subTriangles];
+    return subTriangles;
   }
 
   public subdivide(): void {
@@ -101,22 +101,33 @@ class Icosahedron {
   }
 
   public findEquilateralTriangle(): Triangle {
+    const fractionDigits = 8;
     const equilateralTriangle = this.triangles.find((tr) => {
-      const edge1 = tr.p1().subtract(tr.p2()).length();
-      const edge2 = tr.p2().subtract(tr.p3()).length();
-      const edge3 = tr.p3().subtract(tr.p1()).length();
-      return (
-        edge1.toFixed(8) === edge2.toFixed(8) &&
-        edge2.toFixed(8) === edge3.toFixed(8)
-      );
+      const edgeLength1 = tr
+        .p1()
+        .subtract(tr.p2())
+        .length()
+        .toFixed(fractionDigits);
+      const edgeLength2 = tr
+        .p2()
+        .subtract(tr.p3())
+        .length()
+        .toFixed(fractionDigits);
+      const edgeLength3 = tr
+        .p3()
+        .subtract(tr.p1())
+        .length()
+        .toFixed(fractionDigits);
+
+      return edgeLength1 === edgeLength2 && edgeLength2 === edgeLength3;
     });
     if (!equilateralTriangle) {
       // eslint-disable-next-line no-console
-      console.warn(false, 'Equilateral Triangle not found');
+      console.warn('Equilateral Triangle not found');
       // eslint-disable-next-line no-debugger
       debugger;
     }
-    return equilateralTriangle || this.triangles[0];
+    return equilateralTriangle || this.triangles[0]; // this.triangles[0] shouldn't ever been returned
   }
 }
 
