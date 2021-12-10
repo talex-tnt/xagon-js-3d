b71b71c6d2157929cfac53c21fa64b88d92ebcfe
Nicola <nicola.porta@spindox.it>
2021-12-10 14:54:06 +0100

    - merge develop into branch - refctoring

--- xagon-react-app/src/components/GameComponent/inputManager/FlipGesture.ts
+++ xagon-react-app/src/components/GameComponent/inputManager/FlipGesture.ts
@@ -128,11 +128,11 @@ class FlipGesture extends Gesture {
 
               const swapType = (trM1?: TriangleMesh, trM2?: TriangleMesh) => {
                 if (trM1 && trM2) {
-                  const tr1 = trM1.getTriangle();
-                  const tr2 = trM2.getTriangle();
-                  const tr1Type = tr1.getType();
-                  tr1.setType(tr2.getType());
-                  tr2.setType(tr1Type);
+                  const trFirst = trM1.getTriangle();
+                  const trSecond = trM2.getTriangle();
+                  const tr1Type = trFirst.getType();
+                  trFirst.setType(trSecond.getType());
+                  trSecond.setType(tr1Type);
 
                   const hasPoint = (tr: AdjacentTriangle, point: Vector3) =>
                     tr &&
@@ -146,10 +146,15 @@ class FlipGesture extends Gesture {
                   ) => array.find((e) => e.getId() === element.getId());
 
                   // hexagon tr1
-                  {
+                  const hexagonCompleted = (
+                    first: TriangleMesh,
+                    second: TriangleMesh,
+                  ) => {
+                    const tr1 = first.getTriangle();
+                    const tr2 = second.getTriangle();
                     const hexagon: Triangle[] = [];
 
-                    const map = Object.keys(trM1.getAdjacentsVerticesMap(tr2));
+                    const map = Object.keys(first.getAdjacentsVerticesMap(tr2));
 
                     const notAdjacentIndex = [0, 1, 2].filter(
                       (index) =>
@@ -214,81 +219,161 @@ class FlipGesture extends Gesture {
                     }
 
                     if (verifedDirections === 2 && hexagon.length === 6) {
-                      console.log('WINNER');
+                      return true;
                     }
+                    return false;
+                  };
+
+                  if (hexagonCompleted(trM1, trM2)) {
+                    console.log('winner');
                   }
+                  if (hexagonCompleted(trM2, trM1)) {
+                    console.log('winner');
+                  }
+
+                  // {
+                  //   const hexagon: Triangle[] = [];
+
+                  //   const map = Object.keys(trM1.getAdjacentsVerticesMap(tr2));
+
+                  //   const notAdjacentIndex = [0, 1, 2].filter(
+                  //     (index) =>
+                  //       index !== Number(map[0]) && index !== Number(map[1]),
+                  //   );
+
+                  //   const notAdjacentPoint =
+                  //     notAdjacentIndex &&
+                  //     tr1.getVertices()[notAdjacentIndex[0]];
+
+                  //   let verifedDirections = 0;
+                  //   hexagon.push(tr1);
+
+                  //   const adjs = tr1
+                  //     .getAdjacents()
+                  //     .filter(
+                  //       (adj) =>
+                  //         adj?.getId() !== tr2.getId() &&
+                  //         adj?.getType() === tr1.getType(),
+                  //     );
+
+                  //   if (adjs[0] && adjs[1] && adjs.length === 2) {
+                  //     hexagon.push(adjs[0]);
+                  //     hexagon.push(adjs[1]);
+                  //     adjs.forEach((a) => {
+                  //       const adjs2 = a
+                  //         ?.getAdjacents()
+                  //         .find(
+                  //           (adj2) =>
+                  //             adj2?.getType() === tr1.getType() &&
+                  //             adj2.getId() !== tr1.getId() &&
+                  //             hasPoint(adj2, notAdjacentPoint),
+                  //         );
+
+                  //       if (adjs2) {
+                  //         if (!hasDuplicates(hexagon, adjs2)) {
+                  //           hexagon.push(adjs2);
+                  //         }
+
+                  //         if (a && hasPoint(adjs2, notAdjacentPoint)) {
+                  //           const adjs3 = adjs2
+                  //             ?.getAdjacents()
+                  //             .find(
+                  //               (adj3) =>
+                  //                 adj3?.getType() === tr1.getType() &&
+                  //                 adj3.getId() !== a.getId() &&
+                  //                 hasPoint(adj3, notAdjacentPoint),
+                  //             );
+
+                  //           if (adjs3 && hasPoint(adjs3, notAdjacentPoint)) {
+                  //             if (
+                  //               verifedDirections === 0 &&
+                  //               !hasDuplicates(hexagon, adjs3)
+                  //             ) {
+                  //               hexagon.push(adjs3);
+                  //             }
+                  //             verifedDirections += 1;
+                  //           }
+                  //         }
+                  //       }
+                  //     });
+                  //   }
+
+                  //   if (verifedDirections === 2 && hexagon.length === 6) {
+                  //     console.log('WINNER');
+                  //   }
+                  // }
 
                   // hexagon tr2
-                  {
-                    const hexagon: Triangle[] = [];
-                    const map = Object.keys(trM2.getAdjacentsVerticesMap(tr1));
+                  // {
+                  //   const hexagon: Triangle[] = [];
+                  //   const map = Object.keys(trM2.getAdjacentsVerticesMap(tr1));
 
-                    const notAdjacentIndex = [0, 1, 2].filter(
-                      (index) =>
-                        index !== Number(map[0]) && index !== Number(map[1]),
-                    );
+                  //   const notAdjacentIndex = [0, 1, 2].filter(
+                  //     (index) =>
+                  //       index !== Number(map[0]) && index !== Number(map[1]),
+                  //   );
 
-                    const notAdjacentPoint =
-                      notAdjacentIndex &&
-                      tr2.getVertices()[notAdjacentIndex[0]];
+                  //   const notAdjacentPoint =
+                  //     notAdjacentIndex &&
+                  //     tr2.getVertices()[notAdjacentIndex[0]];
 
-                    let verifedDirections = 0;
-                    hexagon.push(tr2);
+                  //   let verifedDirections = 0;
+                  //   hexagon.push(tr2);
 
-                    const adjs = tr2
-                      .getAdjacents()
-                      .filter(
-                        (adj) =>
-                          adj?.getId() !== tr1.getId() &&
-                          adj?.getType() === tr2.getType(),
-                      );
+                  //   const adjs = tr2
+                  //     .getAdjacents()
+                  //     .filter(
+                  //       (adj) =>
+                  //         adj?.getId() !== tr1.getId() &&
+                  //         adj?.getType() === tr2.getType(),
+                  //     );
 
-                    if (adjs[0] && adjs[1] && adjs.length === 2) {
-                      hexagon.push(adjs[0]);
-                      hexagon.push(adjs[1]);
-                      adjs.forEach((a) => {
-                        const adjs2 = a
-                          ?.getAdjacents()
-                          .find(
-                            (adj2) =>
-                              adj2?.getType() === tr2.getType() &&
-                              adj2.getId() !== tr2.getId() &&
-                              hasPoint(adj2, notAdjacentPoint),
-                          );
+                  //   if (adjs[0] && adjs[1] && adjs.length === 2) {
+                  //     hexagon.push(adjs[0]);
+                  //     hexagon.push(adjs[1]);
+                  //     adjs.forEach((a) => {
+                  //       const adjs2 = a
+                  //         ?.getAdjacents()
+                  //         .find(
+                  //           (adj2) =>
+                  //             adj2?.getType() === tr2.getType() &&
+                  //             adj2.getId() !== tr2.getId() &&
+                  //             hasPoint(adj2, notAdjacentPoint),
+                  //         );
 
-                        if (adjs2) {
-                          if (!hasDuplicates(hexagon, adjs2)) {
-                            hexagon.push(adjs2);
-                          }
+                  //       if (adjs2) {
+                  //         if (!hasDuplicates(hexagon, adjs2)) {
+                  //           hexagon.push(adjs2);
+                  //         }
 
-                          if (a && hasPoint(adjs2, notAdjacentPoint)) {
-                            const adjs3 = adjs2
-                              ?.getAdjacents()
-                              .find(
-                                (adj3) =>
-                                  adj3?.getType() === tr2.getType() &&
-                                  adj3.getId() !== a.getId() &&
-                                  hasPoint(adj3, notAdjacentPoint),
-                              );
+                  //         if (a && hasPoint(adjs2, notAdjacentPoint)) {
+                  //           const adjs3 = adjs2
+                  //             ?.getAdjacents()
+                  //             .find(
+                  //               (adj3) =>
+                  //                 adj3?.getType() === tr2.getType() &&
+                  //                 adj3.getId() !== a.getId() &&
+                  //                 hasPoint(adj3, notAdjacentPoint),
+                  //             );
 
-                            if (adjs3 && hasPoint(adjs3, notAdjacentPoint)) {
-                              if (
-                                verifedDirections === 0 &&
-                                !hasDuplicates(hexagon, adjs3)
-                              ) {
-                                hexagon.push(adjs3);
-                              }
-                              verifedDirections += 1;
-                            }
-                          }
-                        }
-                      });
-                    }
+                  //           if (adjs3 && hasPoint(adjs3, notAdjacentPoint)) {
+                  //             if (
+                  //               verifedDirections === 0 &&
+                  //               !hasDuplicates(hexagon, adjs3)
+                  //             ) {
+                  //               hexagon.push(adjs3);
+                  //             }
+                  //             verifedDirections += 1;
+                  //           }
+                  //         }
+                  //       }
+                  //     });
+                  //   }
 
-                    if (verifedDirections === 2 && hexagon.length === 6) {
-                      console.log('WINNER');
-                    }
-                  }
+                  //   if (verifedDirections === 2 && hexagon.length === 6) {
+                  //     console.log('WINNER');
+                  //   }
+                  // }
                 }
               };
 
