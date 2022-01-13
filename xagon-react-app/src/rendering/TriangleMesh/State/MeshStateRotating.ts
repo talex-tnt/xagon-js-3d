b248022a4c4206dde92b706af201844ac4f13095
Nicola <nicola.porta@spindox.it>
2022-01-13 10:46:59 +0100

    +  methods to compute Mesh's transformations

--- xagon-react-app/src/rendering/TriangleMesh/State/MeshStateRotating.ts
+++ xagon-react-app/src/rendering/TriangleMesh/State/MeshStateRotating.ts
@@ -24,6 +24,8 @@ class MeshStateRotating extends IMeshState {
 
   private mesh: TriangleMesh;
 
+  private adjacentMesh: TriangleMesh;
+
   private rotationAxis: Vector3 = Vector3.Zero();
 
   private rotationAngle = 0;
@@ -69,162 +71,114 @@ class MeshStateRotating extends IMeshState {
     this.scene = scene;
     this.onFlipEnd = onFlipEnd;
     this.mesh = thisTriangleMesh;
+    this.adjacentMesh = adjacentTriangleMesh;
 
-    const mesh = thisTriangleMesh;
     const adjacentMesh = adjacentTriangleMesh;
-    const edges = mesh.getEdges();
-    const vertices = mesh.getVertices();
-
-    const adjacentsVerticesMap = mesh.getAdjacentsVerticesMap(
-      adjacentMesh.getTriangle(),
+    const adjacentsVerticesMap = this.mesh.getAdjacentsVerticesMap(
+      this.adjacentMesh.getTriangle(),
     );
 
-    if (mesh && adjacentMesh) {
-      const triangleMesh = mesh.getTriangleMesh();
+    if (this.mesh && this.adjacentMesh) {
+      const triangleMesh = this.mesh.getTriangleMesh();
       if (triangleMesh) {
         this.scalingNode = triangleMesh.parent as TransformNode;
 
-        if (this.scalingNode) {
-          const flipNode = this.scalingNode.parent as TransformNode;
+        const vertIndices = adjacentsVerticesMap.trAdjs;
 
-          if (edges && vertices) {
-            const vertIndices = adjacentsVerticesMap.trAdjs;
-            const vertIndicesSum = mesh.getTriangleMeshIndicesSum(vertIndices);
+        this.rotationAxis = this.computeRotationAxis(vertIndices);
 
-            const edgeIndex = mesh.getTriangleMeshFlipEdgeIndex(vertIndicesSum);
+        this.flipNode = this.computeFlipNodePosition(vertIndices);
 
-            this.rotationAxis = edges[edgeIndex];
+        const rotationData = this.computeRotationData(vertIndices);
 
-            if (flipNode) {
-              const flipNodeCenter = Vector3.Zero(); // node position in object space
-              const edgeCenterPoint = Vector3.Center(
-                vertices[vertIndices[0]],
-                vertices[vertIndices[1]],
-              );
+        if (rotationData) {
+          if (direction === 1) {
+            this.rotationAngle = -rotationData.rotationDownAngle;
+          } else {
+            this.rotationAngle = Math.PI * 2 - rotationData.rotationDownAngle;
+          }
+          // computing length ratio between vector (edge's center - mesh's center) of the first triangle and vector (edge's center - mesh's center) of the second triangle to shift the scaling node during rotation
+          const scalingNodeShiftRatio =
+            rotationData.adjRotationVector.length() /
+            rotationData.rotationVector.length();
 
-              flipNode.setPositionWithLocalVector(edgeCenterPoint);
-              this.scalingNode.position = flipNodeCenter.subtract(
-                flipNode.position,
-              );
-              this.flipNode = flipNode;
+          const centerShiftVector =
+            this.computeCenterShiftVector(adjacentsVerticesMap);
 
-              const rotationData = this.computeRotationData(
-                adjacentMesh,
-                vertIndices,
-              );
+          this.scalingNodeOrigPos = this.scalingNode.position.clone();
+          this.scalingNodeFinPos = this.scalingNodeOrigPos
+            .scale(scalingNodeShiftRatio)
+            .subtract(centerShiftVector);
 
-              if (rotationData) {
-                if (direction === 1) {
-                  this.rotationAngle = -rotationData.rotationDownAngle;
-                } else {
-                  this.rotationAngle =
-                    Math.PI * 2 - rotationData.rotationDownAngle;
-                }
-                // computing length ratio between vector (edge's center - mesh's center) of the first triangle and vector (edge's center - mesh's center) of the second triangle to shift the scaling node during rotation
-                const scalingNodeShiftRatio =
-                  rotationData.adjRotationVector.length() /
-                  rotationData.rotationVector.length();
+          this.bonesIndices = this.getBonesIndices(vertIndices);
 
-                if (adjacentMesh) {
-                  const centerShiftVector = this.computeCenterShiftVector(
-                    adjacentMesh,
-                    adjacentsVerticesMap,
-                  );
+          this.skeleton =
+            triangleMesh &&
+            triangleMesh.skeleton &&
+            triangleMesh.skeleton.bones;
 
-                  this.scalingNodeOrigPos = this.scalingNode.position.clone();
-                  this.scalingNodeFinPos = this.scalingNodeOrigPos
-                    .scale(scalingNodeShiftRatio)
-                    .subtract(centerShiftVector);
+          this.bonesScaling = this.getBonesScaling();
 
-                  this.bonesIndices = this.getBonesIndices(vertIndices);
+          const adjVertIndices = adjacentsVerticesMap.adjTrAdjs;
+          this.adjBonesScalingY = this.computeAdjBonesScalingY(adjVertIndices);
 
-                  this.skeleton =
-                    triangleMesh &&
-                    triangleMesh.skeleton &&
-                    triangleMesh.skeleton.bones;
+          const adjNotAdjVertexIndex = [0, 1, 2].findIndex(
+            (e) => e !== adjVertIndices[0] && e !== adjVertIndices[1],
+          );
 
-                  this.bonesScaling = this.getBonesScaling();
+          let vertIndex = vertIndices.findIndex((v) => v === 0);
+          if (vertIndex === -1) {
+            vertIndex = adjNotAdjVertexIndex;
+          } else {
+            vertIndex = adjVertIndices[vertIndex];
+          }
 
-                  const adjVertIndices = adjacentsVerticesMap.adjTrAdjs;
-                  const adjNotAdjVertexIndex = [0, 1, 2].findIndex(
-                    (e) => e !== adjVertIndices[0] && e !== adjVertIndices[1],
-                  );
-                  let vertIndex = vertIndices.findIndex((v) => v === 0);
-                  if (vertIndex === -1) {
-                    vertIndex = adjNotAdjVertexIndex;
-                  } else {
-                    vertIndex = adjVertIndices[vertIndex];
-                  }
+          this.nodeRotationAngle = this.computeNodeRotationAngle(vertIndex);
 
-                  this.adjBonesScalingY = this.computeAdjBonesScalingY(
-                    adjacentMesh,
-                    adjVertIndices,
-                  );
+          this.bonesDeformation = this.computeBonesDeformation(vertIndex);
 
-                  this.nodeRotationAngle = this.computeNodeRotationAngle(
-                    adjacentMesh,
-                    vertIndex,
-                  );
-
-                  this.bonesDeformation = this.computeBonesDeformation(
-                    adjacentMesh,
-                    vertIndex,
-                  );
-
-                  if (DEBUG_RENDERING) {
-                    // eslint-disable-next-line no-console
-                    console.log(
-                      'TriangleID - ',
-                      this.mesh.getTriangle().getId(),
-                      'AdjacentTriangleID - ',
-                      adjacentMesh.getTriangle().getId(),
-                    );
-                    const meshSPHERE1 = MeshBuilder.CreateSphere(
-                      `tr1${this.mesh.getTriangle().getName()}`,
-                      {
-                        diameter: 0.1,
-                      },
-                    );
-                    meshSPHERE1.parent = this.scalingNode;
-                    meshSPHERE1.position = vertices[0].scale(1.5);
-                    const mat1 = new StandardMaterial(
-                      `color${vertices[0]}`,
-                      scene,
-                    );
-                    mat1.diffuseColor = new Color3(0, 0, 1);
-                    meshSPHERE1.material = mat1;
-                    const meshSPHERE2 = MeshBuilder.CreateSphere(
-                      `tr2${this.mesh.getTriangle().getName()}`,
-                      {
-                        diameter: 0.1,
-                      },
-                    );
-                    meshSPHERE2.parent = this.scalingNode;
-                    meshSPHERE2.position = vertices[1].scale(1.5);
-                    const mat2 = new StandardMaterial(
-                      `color${vertices[1]}`,
-                      scene,
-                    );
-                    mat2.diffuseColor = new Color3(0, 1, 0);
-                    meshSPHERE2.material = mat2;
-                    const meshSPHERE3 = MeshBuilder.CreateSphere(
-                      `tr3${this.mesh.getTriangle().getName()}`,
-                      {
-                        diameter: 0.1,
-                      },
-                    );
-                    meshSPHERE3.parent = this.scalingNode;
-                    meshSPHERE3.position = vertices[2].scale(1.5);
-                    const mat3 = new StandardMaterial(
-                      `color${vertices[2]}`,
-                      scene,
-                    );
-                    mat3.diffuseColor = new Color3(1, 0, 0);
-                    meshSPHERE3.material = mat3;
-                  }
-                }
-              }
-            }
+          if (DEBUG_RENDERING) {
+            const vertices = this.mesh.getVertices();
+            // eslint-disable-next-line no-console
+            console.log(
+              'TriangleID - ',
+              this.mesh.getTriangle().getId(),
+              'AdjacentTriangleID - ',
+              adjacentMesh.getTriangle().getId(),
+            );
+            const meshSPHERE1 = MeshBuilder.CreateSphere(
+              `tr1${this.mesh.getTriangle().getName()}`,
+              {
+                diameter: 0.1,
+              },
+            );
+            meshSPHERE1.parent = this.scalingNode;
+            meshSPHERE1.position = vertices[0].scale(1.5);
+            const mat1 = new StandardMaterial(`color${vertices[0]}`, scene);
+            mat1.diffuseColor = new Color3(0, 0, 1);
+            meshSPHERE1.material = mat1;
+            const meshSPHERE2 = MeshBuilder.CreateSphere(
+              `tr2${this.mesh.getTriangle().getName()}`,
+              {
+                diameter: 0.1,
+              },
+            );
+            meshSPHERE2.parent = this.scalingNode;
+            meshSPHERE2.position = vertices[1].scale(1.5);
+            const mat2 = new StandardMaterial(`color${vertices[1]}`, scene);
+            mat2.diffuseColor = new Color3(0, 1, 0);
+            meshSPHERE2.material = mat2;
+            const meshSPHERE3 = MeshBuilder.CreateSphere(
+              `tr3${this.mesh.getTriangle().getName()}`,
+              {
+                diameter: 0.1,
+              },
+            );
+            meshSPHERE3.parent = this.scalingNode;
+            meshSPHERE3.position = vertices[2].scale(1.5);
+            const mat3 = new StandardMaterial(`color${vertices[2]}`, scene);
+            mat3.diffuseColor = new Color3(1, 0, 0);
+            meshSPHERE3.material = mat3;
           }
         }
       }
@@ -313,16 +267,44 @@ class MeshStateRotating extends IMeshState {
   }
 
   public getRotationSpeed(): number {
-    const rpm = 5;
+    const rpm = 30;
     const rotationSpeed = (rpm / 60) * Math.PI * 2;
     return rotationSpeed;
   }
 
+  private computeRotationAxis(vertIndices: number[]): Vector3 {
+    const vertIndicesSum = this.mesh.getTriangleMeshIndicesSum(vertIndices);
+    const edgeIndex = this.mesh.getTriangleMeshFlipEdgeIndex(vertIndicesSum);
+    const edges = this.mesh.getEdges() as Vector3[];
+
+    return edges[edgeIndex];
+  }
+
+  private computeFlipNodePosition(
+    vertIndices: number[],
+  ): Nullable<TransformNode> {
+    if (this.scalingNode) {
+      const flipNode = this.scalingNode.parent as TransformNode;
+      const vertices = this.mesh.getVertices();
+      if (flipNode && vertices) {
+        const flipNodeCenter = Vector3.Zero(); // node position in object space
+        const edgeCenterPoint = Vector3.Center(
+          vertices[vertIndices[0]],
+          vertices[vertIndices[1]],
+        );
+
+        flipNode.setPositionWithLocalVector(edgeCenterPoint);
+        this.scalingNode.position = flipNodeCenter.subtract(flipNode.position);
+        return flipNode;
+      }
+    }
+    return null;
+  }
+
   private computeCenterShiftVector(
-    adjMesh: TriangleMesh,
     verticesMap: Record<string, number[]>,
   ): Vector3 {
-    const adjVertices = adjMesh.getVertices();
+    const adjVertices = this.adjacentMesh.getVertices();
     const vertices = this.mesh.getVertices();
     const vertIndices = verticesMap.trAdjs;
     const adjVertIndices = verticesMap.adjTrAdjs;
@@ -380,18 +362,17 @@ class MeshStateRotating extends IMeshState {
     return [Vector3.One(), Vector3.One(), Vector3.One()];
   }
 
-  private computeAdjBonesScalingY(
-    mesh: TriangleMesh,
-    indices: number[],
-  ): number[] {
+  private computeAdjBonesScalingY(indices: number[]): number[] {
     let adjBonesScalingY = [1, 1, 1];
-    const triangleMesh = mesh.getTriangleMesh();
+    const adjTriangleMesh = this.adjacentMesh.getTriangleMesh();
 
-    if (triangleMesh) {
-      const skeleton = triangleMesh.skeleton && triangleMesh.skeleton.bones;
+    if (adjTriangleMesh) {
+      const skeleton =
+        adjTriangleMesh.skeleton && adjTriangleMesh.skeleton.bones;
 
       if (skeleton) {
-        const bonesIndices = mesh.getTriangleMeshBonesIndices(indices);
+        const bonesIndices =
+          this.adjacentMesh.getTriangleMeshBonesIndices(indices);
         const notAdjBoneIndex = [0, 1, 2].findIndex(
           (e) => e !== bonesIndices[0] && e !== bonesIndices[1],
         );
@@ -406,10 +387,7 @@ class MeshStateRotating extends IMeshState {
     return adjBonesScalingY;
   }
 
-  private computeNodeRotationAngle(
-    adjMesh: TriangleMesh,
-    vertIndex: number,
-  ): number {
+  private computeNodeRotationAngle(vertIndex: number): number {
     const matrix = this.computeMatrix();
 
     const centerPointAfterRotation = Vector3.TransformCoordinates(
@@ -423,7 +401,7 @@ class MeshStateRotating extends IMeshState {
         Vector3.TransformCoordinates(v, matrix),
       );
 
-      const adjTriangle = adjMesh.getTriangle();
+      const adjTriangle = this.adjacentMesh.getTriangle();
       const adjTriangleCenter = adjTriangle.getCenterPoint();
       const adjTriangleVertices = adjTriangle.getVertices();
       const adjPointCenterVector =
@@ -474,13 +452,13 @@ class MeshStateRotating extends IMeshState {
     return Matrix.Zero();
   }
 
-  public computeBonesDeformation(mesh: TriangleMesh, index: number): number[] {
+  public computeBonesDeformation(index: number): number[] {
     const customTriangleVertices = this.computeCustomTriangleVertices(
-      mesh,
+      this.adjacentMesh,
       index,
     );
 
-    const bonesRotations = mesh.computeAngleBonesRotation({
+    const bonesRotations = this.adjacentMesh.computeAngleBonesRotation({
       triangle: new Triangle(
         BigInt(0),
         customTriangleVertices[0],
@@ -525,10 +503,7 @@ class MeshStateRotating extends IMeshState {
     return customTriangleVertices;
   }
 
-  public computeRotationData(
-    adjMesh: TriangleMesh,
-    indices: number[],
-  ): Nullable<{
+  public computeRotationData(indices: number[]): Nullable<{
     rotationVector: Vector3;
     adjRotationVector: Vector3;
     rotationDownAngle: number;
@@ -544,7 +519,7 @@ class MeshStateRotating extends IMeshState {
         const triangleMeshMatrix = triangleMesh.computeWorldMatrix(true);
 
         const centerADJ = Vector3.TransformCoordinates(
-          adjMesh.getTriangle().getCenterPoint(),
+          this.adjacentMesh.getTriangle().getCenterPoint(),
           Matrix.Invert(triangleMeshMatrix),
         );
 
@@ -580,7 +555,7 @@ class MeshStateRotating extends IMeshState {
           .getTriangle()
           .getCenterPoint()
           .subtract(worldSpaceAdjEdgeCenterPoint);
-        const adjRotationVector = adjMesh
+        const adjRotationVector = this.adjacentMesh
           .getTriangle()
           .getCenterPoint()
           .subtract(worldSpaceAdjEdgeCenterPoint);
